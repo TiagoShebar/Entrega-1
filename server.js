e5ef894157b11c2465b5d2fe20fb752b58f56910
eitan <mail@mail.com>
2024-06-07 09:53:39 -0300

    los ultimos puntos estan agregados en comentarios

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import UsersController from './src/controllers/users-controller.js';
 import ProvincesController from './src/controllers/provinces-controller.js';
 import EventCategoryController from './src/controllers/event_category-controller.js';
 import LocationController from './src/controllers/location-controller.js';
+import EventLocationController from './src/controllers/event_location-controller.js';
 
 const app = express(); //Init API REST
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use("/api/user", UsersController);
 app.use("/api/province", ProvincesController); 
 app.use("/api/event-category", EventCategoryController); 
 app.use("/api/location", LocationController); 
+app.use("/api/event-location", EventLocationController);
 //app.use(ErrorMiddleware);
 app.listen(port, () => {
     console.log(`Server is runing on port ${port}`);
